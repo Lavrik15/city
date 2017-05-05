aeba2d33cfd8e3c59b525fdca9ee9f562facbb96
Lavrik <lavrik.aw@ya.ru>
2017-05-05 15:14:43 +0300

    popup

---
+++ src/js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	var promoItem = $(".promo-item");
+	var popup = $(".popup");
+	var backdor = $(".backdor-popup");
+
+	
+	promoItem.on("click", popupOpen);
+
+	$(".popup-close").on("click", close);
+	$(".btn-link").on("click", close);
+
+	function popupOpen() {
+		popup.show();
+		backdor.show();
+	}
+	function close() {
+		backdor.hide();
+		popup.hide();
+	}
+});
