2d64f6984d07f37af7c84b7a4ad845ca6052eabb
Lavrik <lavrik.aw@ya.ru>
2017-05-06 09:20:57 +0300

    no message

--- src/js/common.js
+++ src/js/common.js
@@ -1,13 +1,116 @@
 $(document).ready(function(){
+	// popup
 	var promoItem = $(".promo-item");
 	var popup = $(".popup");
 	var backdor = $(".backdor-popup");
-
 	
 	promoItem.on("click", popupOpen);
 
 	$(".popup-close").on("click", close);
 	$(".btn-link").on("click", close);
+
+
+	// form 
+	// email validation
+	var success = {
+		"border" : "1px solid #4488bb",
+		"color" : "#4488bb"
+	}
+	var error = {
+		"border" : "1px solid #bb4444",
+		"color" : "#bb4444"
+	}
+	var emailInput = $("input[type='email']");
+	var emailInputValidate = false;
+
+	emailInput.on("change", function(){
+		var $this = $(this);
+		var inputVal = $this.val();
+
+		if (validateEmail(inputVal)) {
+			emailInput.css(success);
+			$this.siblings(".input-label").css({
+				"color" : "#4488bb"
+			});
+			$this.siblings(".form-tooltip").hide();
+			emailInputValidate = true;
+		} else {
+			emailInput.css(error);
+			$this.siblings(".input-label").css({
+				"color" : "#bb4444"
+			});
+			$this.siblings(".form-tooltip").show();
+		}
+	});
+
+	// Name validation
+	var nameInput = $("input[name='name']");
+	var nameInputValidate = false;
+
+	nameInput.on("change", function(){
+		var $this = $(this);
+		var inputVal = $this.val();
+
+		if (validateName(inputVal)) {
+			nameInput.css(success);
+			$this.siblings(".input-label").css({
+				"color" : "#4488bb"
+			});
+			$this.siblings(".form-tooltip").hide();
+			nameInputValidate = true;
+		} else {
+			nameInput.css(error);
+			$this.siblings(".input-label").css({
+				"color" : "#bb4444"
+			});
+			$this.siblings(".form-tooltip").show();
+
+		}
+	});
+
+	// send form
+	var selectCountry = $(".country");
+	var agreeCheckbox = $("#agree");
+	$(".btn-js").on("click", function(e){
+		var $this = $(this);
+		var optionReq = $(".country option"), resultOption, resultCheckbox;
+		
+		for(i = 1; i < optionReq.length; i++) {
+		    if(optionReq[i].selected) {
+		        resultOption = true;
+		        break;
+		    }
+		}
+		if ( (agreeCheckbox).is(':checked') ) {
+			resultCheckbox = true;
+			console.log('ghbdtn');
+		} 
+		if( !resultOption || !resultCheckbox || !nameInputValidate || !emailInputValidate) {
+			e.preventDefault();
+			if (!resultOption) {
+				console.log(optionReq);
+				$(".country").css(error);
+			}
+		    if (!resultCheckbox) {
+		    	$(".agree-label").css({
+		    		"color" : "#bb4444"
+		    	});
+		    }
+		    if (!nameInputValidate) {
+		    	nameInput.css(error);
+		    }
+		    if (!emailInputValidate) {
+		    	emailInput.css(error);
+		    }
+		   	// selectCountry.siblings(".form-tooltip").show();
+		}
+	});
+	selectCountry.on("change", function(){
+		$(this).css(success);
+	});
+	$(".agree-label").on("click", function(){
+		$(this).attr("style", "");
+	});
 
 	function popupOpen() {
 		popup.show();
@@ -16,5 +119,13 @@ $(document).ready(function(){
 	function close() {
 		backdor.hide();
 		popup.hide();
+	}
+	function validateEmail(email) {
+	    var re = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+	    return re.test(email);
+	}
+	function validateName(name) {
+		var re = "/^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u";
+		return re.test(name);
 	}
 });
